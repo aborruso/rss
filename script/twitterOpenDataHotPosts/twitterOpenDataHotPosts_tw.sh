7e469d6b627ec7ede64385ad640ed4d94219d209
Andrea Borruso <aborruso@gmail.com>
2021-08-27 16:13:18 +0200

    update

--- script/twitterOpenDataHotPosts/twitterOpenDataHotPosts_tw.sh
+++ script/twitterOpenDataHotPosts/twitterOpenDataHotPosts_tw.sh
@@ -28,14 +28,18 @@ mkdir -p "$folder"/rawdata
 mkdir -p "$folder"/processing
 mkdir -p "$folder"/../../docs/"$nome"
 
+# se lo script funziona sul PC di andy leggi config locale
 if [[ $(hostname) == "DESKTOP-7NVNDNF" ]]; then
   source "$folder"/.config
 fi
 
+# query per scaricare i tweet caldi a tema Open Data
 query='(min_retweets:10) OR (min_faves:10) opendata OR ("open data") OR ("dati aperti") OR ("dati pubblici") OR ("dato aperto") OR ("dato pubblico") OR ("données publiques") OR ("Verwaltungsdaten festgelegt") OR ("offene Verwaltungsdaten") OR ("DATOS ABIERTOS") OR ("avoin data") OR "avoindata" OR "datosabiertos" OR DadesObertes OR dadosabertos OR ("dados abertos")'
 
+# scarica i tweet a partire dalla query di sopra
 twarc --consumer_key "$CONSUMER_KEY" --consumer_secret "$CONSUMER_SECRET" --access_token "$ACCESS_TOKEN" --access_token_secret "$ACCESS_TOKEN_SECRET" search "$query" >"$folder"/rawdata/latest.jsonl
 
+# converti i tweet scaricati da JSON a CSV
 mlr --j2c cut -o -f id,lang,created_at,full_text then put -S '$URL="https://twitter.com/user/status/".$id' "$folder"/rawdata/latest.jsonl >"$folder"/rawdata/latest.csv
 
 # crea archivio tweet
@@ -51,7 +55,7 @@ fi
 # estrai righe da pubblicare
 mlr --csv head -n 100 "$folder"/processing/archive.csv >"$folder"/rawdata/tmp_pub.csv
 
-# crea file di check se non esiste
+# se non esiste, crea file vuoto per archiviare id dei tweet che sono già stati tradotti in italiano
 if [ ! -f "$folder"/processing/ita_check.csv ]; then
   touch "$folder"/processing/ita_check.csv
 fi
@@ -68,7 +72,7 @@ else
   echo "non traduco"
 fi
 
-# se il file di archivio con le traduzioni non esiste, crealo
+# se il file di archivio con le traduzioni in italiano non esiste, creane uno vuoto
 if [ ! -f "$folder"/processing/archive_ita.csv ]; then
   touch "$folder"/processing/archive_ita.csv
 fi
@@ -77,22 +81,22 @@ fi
 mlr --csv uniq -a then sort -nr id "$folder"/rawdata/tmp_pub_ita.csv "$folder"/processing/archive_ita.csv >"$folder"/rawdata/tmp.csv
 mv "$folder"/rawdata/tmp.csv "$folder"/processing/archive_ita.csv
 
-# aggiorna file check traduzioni
+# aggiorna file con gli id dei tweet già tradotti
 mlr --csv filter -S '$titolo=~".+"' then cut -f id then uniq -a then sort -nr id "$folder"/processing/archive_ita.csv >"$folder"/processing/ita_check.csv
 
 # RSS
 
 # estrai dati per feed in italiano
-mlr --csv head -n 100 then put -S '$rssDate = strftime(strptime($created_at, "%a %b %d %H:%M:%S +0000 %Y"),"%Y-%m-%dT%H:%M:%SZ")' then cut -o -f titolo,URL,rssDate,id,lang then label description,link,pubDate,title,lang then put '$guid=$link' then put -S '$title="#".$lang." @ ".$title' then cut -x -f lang "$folder"/processing/archive_ita.csv>"$folder"/rawdata/data_ita.csv
+mlr --csv head -n 100 then put -S '$rssDate = strftime(strptime($created_at, "%a %b %d %H:%M:%S +0000 %Y"),"%Y-%m-%dT%H:%M:%SZ")' then cut -o -f titolo,URL,rssDate,id,lang then label description,link,pubDate,title,lang then put '$guid=$link' then put -S '$title="#".$lang." @ ".$title' then cut -x -f lang "$folder"/processing/archive_ita.csv >"$folder"/rawdata/data_ita.csv
 
-# rimuovi righe con traduzion non fatta
+# rimuovi righe con traduzione non fatta
 mlr -I --csv filter -S '$description=~".+"' "$folder"/rawdata/data_ita.csv
 mlr -I --csv filter -S '$titolo=~".+"' "$folder"/processing/archive_ita.csv
 
-# crea feed RSS
+# crea feed RSS con testi dei tweet tradotti in italiano
 ogr2ogr -f geoRSS -dsco TITLE="$titolo" -dsco LINK="$selflink" -dsco DESCRIPTION="$descrizione" -dsco AUTHOR_NAME="$AUTHOR_NAME" "$folder"/../../docs/"$nome"/"$nome"_ita.xml "$folder"/rawdata/data_ita.csv -oo AUTODETECT_TYPE=YES
 
-# crea fee RSS in lingua orignale
+# crea fee RSS in lingua originale
 mlr --csv head -n 100 then rename full_text,description,URL,link,id,title then put -S '$guid=$link;$title="@ ".$title;$pubDate = strftime(strptime($created_at, "%a %b %d %H:%M:%S +0000 %Y"),"%Y-%m-%dT%H:%M:%SZ")' then put -S '$title="#".$lang." ".$title' then cut -x -f created_at,lang "$folder"/processing/archive.csv >"$folder"/rawdata/data.csv
 
 ogr2ogr -f geoRSS -dsco TITLE="$title" -dsco LINK="$selflinkraw" -dsco DESCRIPTION="$description" -dsco AUTHOR_NAME="$AUTHOR_NAME" "$folder"/../../docs/"$nome"/"$nome"_raw.xml "$folder"/rawdata/data.csv -oo AUTODETECT_TYPE=YES
